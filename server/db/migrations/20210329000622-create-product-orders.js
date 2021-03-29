15bd9ec72d5645825a08ccdd496c4a5094bdf725
Dunia <dunia07@hotmail.com>
2021-03-29 16:58:31 -0300

    feat(models): create models

---
+++ server/db/migrations/20210329000622-create-product-orders.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ProductOrders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_id: {
+        references: { model: 'Orders', key: 'id' },
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        references: { model: 'Products', key: 'id' },
+        type: Sequelize.INTEGER
+      },
+      qtd: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ProductOrders');
+  }
+};
